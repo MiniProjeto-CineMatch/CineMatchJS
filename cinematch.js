3f532747d5a4a272cd24f82efb60fc2877c33853
StefaniHermel <contato.hermel@gmail.com>
2026-08-04 20:17:13 -0300

    fix: corrige fechamento da classe Conteudo e padroniza atributo genero

---
+++ cinematch.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')({sigint: true});
+// Classes RF 09, RF10, RF 11.
+class Conteudo {
+    constructor(id, titulo, tipo, genero, duracaoMinutos) {
+        this.id = id;
+        this.titulo = titulo;
+        this.tipo = tipo;
+        this.genero = genero;
+        this.duracaoMinutos = duracaoMinutos;
+    }
+     //uso do "this" para acessar as propriedades do objeto atual
+    exibirResumo() {
+        return `${this.titulo} (${this.tipo}) - ${this.genero}, ${this.duracaoMinutos} minutos`;
+    }
+
+}
+
+// herança: Filme e Serie herdam de Conteudo
+class Serie extends Conteudo {
+    constructor(id, titulo, genero, duracaoMinutos, temporadas) {
+        super(id, titulo, 'Série', genero, duracaoMinutos);
+        this.temporadas = temporadas;
+    }
+    exibirTemporadas() {
+        return `${this.titulo} - ${this.temporadas} temporadas`;
+    }
+}
